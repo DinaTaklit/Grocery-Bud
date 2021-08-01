b0059a51eec98c2bdbcd02579fefabac27d6b98d
DinaTaklit <ez_taklit@esi.dz>
2021-08-01 09:32:21 +0100

    feat: Alert: implement Alert Component

--- src/Alert.js
+++ src/Alert.js
@@ -1,7 +1,20 @@
 import { useEffect } from 'react'
 
-const Alert = () => {
-  return <h2>alert component</h2>
+const Alert = ({type, msg, showAlert, list}) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      showAlert()
+    }, 5000)
+
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [list])  
+  return (
+    <p className={`alert alert-${type}`}>
+      {msg}
+    </p>
+  )
 }
 
 export default Alert
